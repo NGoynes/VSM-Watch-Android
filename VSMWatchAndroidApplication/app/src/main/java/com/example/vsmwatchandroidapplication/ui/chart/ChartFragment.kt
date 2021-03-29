9adda9dd0f680306e2ebe38e67a4f05a5767b84c
ngoynes <nate.goynes@gmail.com>
2021-03-29 02:20:12 -0500

    Working charts - same data

--- VSMWatchAndroidApplication/app/src/main/java/com/example/vsmwatchandroidapplication/ui/chart/ChartFragment.kt
+++ VSMWatchAndroidApplication/app/src/main/java/com/example/vsmwatchandroidapplication/ui/chart/ChartFragment.kt
@@ -1,25 +1,25 @@
         package com.example.vsmwatchandroidapplication.ui.chart
 
+import android.graphics.Color
 import android.os.Bundle
-import android.renderscript.ScriptGroup
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.vsmwatchandroidapplication.R
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
-import com.github.mikephil.charting.charts.LineChart
-import java.io.File
-import android.util.Log
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 import java.io.InputStream
 
 class ChartFragment : Fragment() {
 
     private lateinit var chartViewModel: ChartViewModel
-    //private lateinit var lineChart: LineChart
+    private var ppgSeries = LineGraphSeries<DataPoint>()
+    private var ecgSeries = LineGraphSeries<DataPoint>()
+    private var edaSeries = LineGraphSeries<DataPoint>()
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -29,18 +29,83 @@ class ChartFragment : Fragment() {
         chartViewModel =
                 ViewModelProvider(this).get(ChartViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_chart, container, false)
-        /*val textView: TextView = root.findViewById(R.id.text_chart)
-        chartViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
-        val lineChart: LineChart = root.findViewById((R.id.lineChart))
+
+
+        val ppgChart: GraphView = root.findViewById((R.id.ppgChart))
+        val ecgChart: GraphView = root.findViewById((R.id.ecgChart))
+        val edaChart: GraphView = root.findViewById((R.id.edaChart))
+
         readHealthData()
+
+        //PPG PLOT
+        ppgSeries.color = Color.rgb(233, 87, 87)
+        ppgSeries.isDrawBackground = true
+        ppgSeries.backgroundColor = Color.rgb(233, 179, 179)
+        ppgSeries.thickness = 10
+        ppgChart.title = "PPG"
+        ppgChart.gridLabelRenderer.horizontalAxisTitle = "Time (s)"
+        ppgChart.gridLabelRenderer.verticalAxisTitle = "Impedance Real (Ohms)"
+        ppgChart.gridLabelRenderer.numVerticalLabels = 3
+        ppgChart.gridLabelRenderer.numHorizontalLabels = 3
+        ppgChart.viewport.isYAxisBoundsManual = true
+        ppgChart.viewport.setMinY(0.0)
+        ppgChart.viewport.setMaxY(200000.0)
+        ppgChart.viewport.isXAxisBoundsManual = true
+        ppgChart.viewport.setMinX(0.0)
+        ppgChart.viewport.setMaxX(200.0)
+        ppgChart.viewport.isScrollable = true
+        ppgChart.addSeries(ppgSeries)
+
+        //ECG PLOT
+        ecgSeries.color = Color.rgb(233, 87, 87)
+        ecgSeries.isDrawBackground = true
+        ecgSeries.backgroundColor = Color.rgb(233, 179, 179)
+        ecgSeries.thickness = 10
+        ecgChart.title = "ECG"
+        ecgChart.gridLabelRenderer.horizontalAxisTitle = "Time (s)"
+        ecgChart.gridLabelRenderer.verticalAxisTitle = "Impedance Real (Ohms)"
+        ecgChart.gridLabelRenderer.numVerticalLabels = 3
+        ecgChart.gridLabelRenderer.numHorizontalLabels = 3
+        ecgChart.viewport.isYAxisBoundsManual = true
+        ecgChart.viewport.setMinY(0.0)
+        ecgChart.viewport.setMaxY(200000.0)
+        ecgChart.viewport.isXAxisBoundsManual = true
+        ecgChart.viewport.setMinX(0.0)
+        ecgChart.viewport.setMaxX(200.0)
+        ecgChart.viewport.isScrollable = true
+        ecgChart.addSeries(ecgSeries)
+
+        //EDA PLOT
+        edaSeries.color = Color.rgb(233, 87, 87)
+        edaSeries.isDrawBackground = true
+        edaSeries.backgroundColor = Color.rgb(233, 179, 179)
+        edaSeries.thickness = 10
+        edaChart.title = "EDA"
+        edaChart.gridLabelRenderer.horizontalAxisTitle = "Time (s)"
+        edaChart.gridLabelRenderer.verticalAxisTitle = "Impedance Real (Ohms)"
+        edaChart.gridLabelRenderer.numVerticalLabels = 3
+        edaChart.gridLabelRenderer.numHorizontalLabels = 3
+        edaChart.viewport.isYAxisBoundsManual = true
+        edaChart.viewport.setMinY(0.0)
+        edaChart.viewport.setMaxY(200000.0)
+        edaChart.viewport.isXAxisBoundsManual = true
+        edaChart.viewport.setMinX(0.0)
+        edaChart.viewport.setMaxX(200.0)
+        edaChart.viewport.isScrollable = true
+        edaChart.addSeries(edaSeries)
+
+
         return root
     }
 
     private fun readHealthData() {
         val file: InputStream = resources.openRawResource(R.raw.eda)
         val rows: List<List<String>> = csvReader().readAll(file)
-        print(rows)
+        for (i in rows.indices) {
+            val time = rows[i][0].toDouble() / 1000
+            ppgSeries.appendData(DataPoint(time, rows[i][1].toDouble()),true, rows.size)
+            ecgSeries.appendData(DataPoint(time, rows[i][1].toDouble()),true, rows.size)
+            edaSeries.appendData(DataPoint(time, rows[i][1].toDouble()),true, rows.size)
+        }
     }
 }
