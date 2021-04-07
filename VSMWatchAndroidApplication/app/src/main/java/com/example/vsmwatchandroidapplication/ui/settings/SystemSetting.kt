3d1f1fde23e8bbc3e3dbbb61df030df4363427a7
TVisionz <brian.lu62@gmail.com>
2021-04-06 21:18:33 -0500

    Added all the settings pages

---
+++ VSMWatchAndroidApplication/app/src/main/java/com/example/vsmwatchandroidapplication/ui/settings/SystemSetting.kt
@@ -0,0 +1,12 @@
+package com.example.vsmwatchandroidapplication.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vsmwatchandroidapplication.R
+
+class SystemSetting : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_system_setting)
+    }
+}
