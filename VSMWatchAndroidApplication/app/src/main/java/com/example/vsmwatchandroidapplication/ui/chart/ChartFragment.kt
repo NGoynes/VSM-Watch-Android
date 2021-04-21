c9a306b8935ffd7f791c3b753ac1c0fb6487da50
ngoynes <nate.goynes@gmail.com>
2021-04-21 02:39:56 -0500

    working ecg real time chart
    done with four ibuprofen for back pain

--- VSMWatchAndroidApplication/app/src/main/java/com/example/vsmwatchandroidapplication/ui/chart/ChartFragment.kt
+++ VSMWatchAndroidApplication/app/src/main/java/com/example/vsmwatchandroidapplication/ui/chart/ChartFragment.kt
@@ -3,27 +3,39 @@
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.analog.study_watch_sdk.core.packets.stream.ECGDataPacket
 import com.example.vsmwatchandroidapplication.MainActivity
 import com.example.vsmwatchandroidapplication.R
-import com.jjoe64.graphview.GraphView
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.utils.EntryXComparator
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
-import kotlinx.android.synthetic.main.fragment_chart.*
+import org.jetbrains.anko.support.v4.runOnUiThread
+import java.util.*
 
 
 class ChartFragment : Fragment() {
 
     private lateinit var chartViewModel: ChartViewModel
     private var thread: Thread = Thread()
+    private var prevX = 0
+
     //private lateinit var ecgChart: LineChart
     //private var plotData = true
     private var ecgSeries = LineGraphSeries<DataPoint>()
-    private var lastX = 0
+    private lateinit var ecgChart: LineChart
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,7 +43,7 @@ class ChartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         chartViewModel =
-                ViewModelProvider(this).get(ChartViewModel::class.java)
+            ViewModelProvider(this).get(ChartViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_chart, container, false)
 
         (activity as MainActivity)?.supportActionBar?.title = "Chart"
@@ -48,11 +60,65 @@ class ChartFragment : Fragment() {
         //var tempSeries = LineGraphSeries<DataPoint>()
 
         //val ppgChart: GraphView = root.findViewById((R.id.ppgChart))
-        val ecgChart: GraphView = root.findViewById((R.id.ecgChart))
+        ecgChart = root.findViewById((R.id.ecgChart))
         //val edaPhaseChart: GraphView = root.findViewById((R.id.edaPhaseChart))
         //val edaMagChart: GraphView = root.findViewById((R.id.edaMagChart))
         //val accChart: GraphView = root.findViewById((R.id.accChart))
         //val tempChart: GraphView = root.findViewById((R.id.tempChart))
+
+        // enable description text
+        ecgChart.description.isEnabled = true
+
+        // enable touch gestures
+        ecgChart.setTouchEnabled(true)
+
+        // enable scaling and dragging
+        ecgChart.isDragEnabled = true
+        ecgChart.setScaleEnabled(true)
+        ecgChart.setDrawGridBackground(false)
+        ecgChart.isAutoScaleMinMaxEnabled = true
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        ecgChart.setPinchZoom(true)
+
+        // set an alternative background color
+        ecgChart.setBackgroundColor(Color.WHITE)
+
+        val data = LineData()
+        data.setValueTextColor(Color.WHITE)
+
+        // add empty data
+        ecgChart.data = data
+
+        // get the legend (only possible after setting data)
+        val l: Legend = ecgChart.legend
+
+        // modify the legend ...
+        l.form = Legend.LegendForm.LINE
+        l.textColor = Color.WHITE
+
+        val xl: XAxis = ecgChart.xAxis
+        xl.textColor = Color.WHITE
+        xl.setDrawGridLines(true)
+        xl.setAvoidFirstLastClipping(true)
+        xl.isEnabled = true
+
+        val leftAxis: YAxis = ecgChart.axisLeft
+        leftAxis.textColor = Color.WHITE
+        leftAxis.setDrawGridLines(false)
+        //leftAxis.axisMaximum = 10000f
+        //leftAxis.axisMinimum = 0f
+        leftAxis.setLabelCount(5, true)
+        leftAxis.setDrawGridLines(true)
+
+        val rightAxis: YAxis = ecgChart.axisRight
+        rightAxis.isEnabled = false
+
+        ecgChart.axisLeft.setDrawGridLines(true)
+        ecgChart.xAxis.setDrawGridLines(true)
+        ecgChart.setDrawBorders(true)
+
+        feedMultiple()
 
         /*//PPG PLOT
         ppgSeries1.color = Color.rgb(255, 51, 0)
@@ -80,7 +146,7 @@ class ChartFragment : Fragment() {
         ppgChart.addSeries(ppgSeries2)
         ppgChart.addSeries(ppgSeries1)*/
 
-        //ECG PLOT
+        /*//ECG PLOT
         ecgSeries.color = Color.rgb(255, 51, 0)
         ecgSeries.isDrawBackground = true
         ecgSeries.backgroundColor = Color.argb(200, 233, 179, 179)
@@ -89,14 +155,14 @@ class ChartFragment : Fragment() {
         ecgChart.gridLabelRenderer.horizontalAxisTitle = "Time (s)"
         ecgChart.gridLabelRenderer.verticalAxisTitle = "ECG"
         ecgChart.gridLabelRenderer.numVerticalLabels = 3
-        //ecgChart.viewport.isYAxisBoundsManual = true
+        ecgChart.addSeries(ecgSeries)
+        ecgChart.viewport.isYAxisBoundsManual = true
         ecgChart.viewport.isXAxisBoundsManual = true
         ecgChart.viewport.setMinY(0.0)
-        ecgChart.viewport.setMaxY(10.0)
-        /*ecgChart.viewport.setMinX(ecgSeries.lowestValueX)
-        ecgChart.viewport.setMaxX(ecgSeries.highestValueX)*/
-        ecgChart.viewport.isScrollable = true
-        ecgChart.addSeries(ecgSeries)
+        ecgChart.viewport.setMaxY(100000.0)
+        ecgChart.viewport.setMinX(0.0)
+        ecgChart.viewport.setMaxX(60.0)
+        ecgChart.viewport.isScrollable = true*/
 
         /*//EDA MAG PLOT
         edaSeriesMag.color = Color.rgb(255, 51, 0)
@@ -195,8 +261,9 @@ class ChartFragment : Fragment() {
             startActivity(intent)
         }*/
 
-        ecgChart.setOnClickListener{
-            val intent: Intent = Intent(context?.applicationContext, ECGActivity::class.java)
+        ecgChart.setOnClickListener {
+            val intent: Intent =
+                Intent(context?.applicationContext, ECGActivity::class.java)
             startActivity(intent)
         }
 
@@ -209,30 +276,98 @@ class ChartFragment : Fragment() {
             val intent: Intent = Intent(context?.applicationContext, AccActivity::class.java)
             startActivity(intent)
         }
-*/
+        */
         return root
     }
 
-    private fun addEntry() {
-        ecgSeries.appendData(DataPoint(lastX++.toDouble(), Math.random() * 10.0), true, 10)
+    private fun createSet(): LineDataSet? {
+        val set = LineDataSet(null, "Dynamic Data")
+        set.axisDependency = YAxis.AxisDependency.LEFT
+        set.lineWidth = 3f
+        set.color = Color.MAGENTA
+        set.isHighlightEnabled = false
+        set.setDrawValues(false)
+        set.setDrawCircles(false)
+        set.mode = LineDataSet.Mode.CUBIC_BEZIER
+        set.cubicIntensity = 0.2f
+        return set
+    }
+
+    private fun addEntry(ECGdata: ECGDataPacket) {
+        var data: LineData = ecgChart.data
+
+        if (data != null) {
+            var set = data.getDataSetByIndex(0)
+            // set.addEntry(...); // can be called as well
+
+            // set.addEntry(...); // can be called as well
+            if (set == null) {
+                set = createSet()
+                data.addDataSet(set)
+            }
+
+            for (i in ECGdata.payload.streamData) {
+                if (i != null) {
+                    //if (i.timestamp.toFloat() > data.xMax) {
+                    //data.addEntry(Entry(i.timestamp.toFloat(), i.ecgData.toFloat()), 0)
+                    data.addEntry(Entry(prevX++.toFloat(), i.ecgData.toFloat()), 0)
+                    //}
+                }
+            }
+            //data.addEntry(Entry(set.entryCount.toFloat(), (Math.random() * 80).toFloat() + 10f), 0)
+            //Collections.sort(data, EntryXComparator())
+            data.notifyDataChanged()
+
+            // let the chart know it's data has changed
+            ecgChart.notifyDataSetChanged()
+
+            // limit the number of visible entries
+            ecgChart.setVisibleXRangeMaximum(150F)
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT)
+
+            // move to the latest entry
+            ecgChart.moveViewToX(data.getEntryCount().toFloat())
+
+            //println(data.dataSets.toString())
+        }
+    }
+
+    private fun feedMultiple() {
+        if (thread != null) {
+            thread.interrupt()
+        }
+        val ecg = com.example.vsmwatchandroidapplication.watchSdk!!.ecgApplication
+        ecg.setCallback { ECGdata ->
+
+            //Log.d("Connection", "DATA :: ${ECGdata.payload.ecgInfo}")
+            runOnUiThread {
+                addEntry(ECGdata)
+            }
+            try {
+                Thread.sleep(10)
+            } catch (e: InterruptedException) {
+                // TODO Auto-generated catch block
+                e.printStackTrace()
+            }
+        }
+
+        ecg.startSensor()
+        ecg.subscribeStream()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (thread != null) {
+            thread.interrupt()
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        if (thread != null) {
-            thread.interrupt()
-        }
-        thread = Thread {
-            for (i in 0..100) {
-                addEntry()
-                try {
-                    Thread.sleep(10)
-                } catch (e: InterruptedException) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace()
-                }
-            }
-        }
-        thread.start()
+    }
+
+    override fun onDestroy() {
+        thread.interrupt()
+        super.onDestroy()
     }
 }
--- VSMWatchAndroidApplication/app/src/main/java/com/example/vsmwatchandroidapplication/ui/chart/ECGActivity.kt
+++ VSMWatchAndroidApplication/app/src/main/java/com/example/vsmwatchandroidapplication/ui/chart/ECGActivity.kt
@@ -2,53 +2,176 @@ package com.example.vsmwatchandroidapplication.ui.chart
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.analog.study_watch_sdk.core.packets.stream.ECGDataPacket
 import com.example.vsmwatchandroidapplication.R
-import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
-import com.jjoe64.graphview.GraphView
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
-import java.io.InputStream
-import java.lang.Double
 
 class ECGActivity : AppCompatActivity() {
 
+    private var thread: Thread = Thread()
     private var ecgSeries = LineGraphSeries<DataPoint>()
+    private lateinit var ecgChart: LineChart
+    private var prevX = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ecg)
 
-        val ecgChart: GraphView = findViewById((R.id.ecgChartInd))
+        ecgChart= findViewById((R.id.ecgChartInd))
 
-        readHealthData()
+        // enable description text
+        ecgChart.description.isEnabled = true
 
-        //ECG PLOT
-        ecgSeries.color = Color.rgb(255, 51, 0)
-        ecgSeries.isDrawBackground = true
-        ecgSeries.backgroundColor = Color.argb(200, 233, 179, 179)
-        ecgSeries.thickness = 10
-        ecgChart.title = "ECG"
-        ecgChart.gridLabelRenderer.horizontalAxisTitle = "Time (s)"
-        ecgChart.gridLabelRenderer.verticalAxisTitle = "ECG"
-        ecgChart.gridLabelRenderer.numVerticalLabels = 3
-        ecgChart.viewport.isYAxisBoundsManual = true
-        ecgChart.viewport.isXAxisBoundsManual = true
-        ecgChart.viewport.setMinY(ecgSeries.lowestValueY)
-        ecgChart.viewport.setMaxY(ecgSeries.highestValueY)
-        ecgChart.viewport.setMinX(ecgSeries.lowestValueX)
-        ecgChart.viewport.setMaxX(ecgSeries.highestValueX)
-        ecgChart.viewport.isScrollable = true
-        ecgChart.addSeries(ecgSeries)
+        // enable touch gestures
+        ecgChart.setTouchEnabled(true)
+
+        // enable scaling and dragging
+        ecgChart.isDragEnabled = true
+        ecgChart.setScaleEnabled(true)
+        ecgChart.setDrawGridBackground(false)
+        ecgChart.isAutoScaleMinMaxEnabled = true
+
+        // if disabled, scaling can be done on x- and y-axis separately
+        ecgChart.setPinchZoom(true)
+
+        // set an alternative background color
+        ecgChart.setBackgroundColor(Color.WHITE)
+
+        val data = LineData()
+        data.setValueTextColor(Color.WHITE)
+
+        // add empty data
+        ecgChart.data = data
+
+        // get the legend (only possible after setting data)
+        val l: Legend = ecgChart.legend
+
+        // modify the legend ...
+        l.form = Legend.LegendForm.LINE
+        l.textColor = Color.WHITE
+
+        val xl: XAxis = ecgChart.xAxis
+        xl.textColor = Color.WHITE
+        xl.setDrawGridLines(true)
+        xl.setAvoidFirstLastClipping(true)
+        xl.isEnabled = true
+
+        val leftAxis: YAxis = ecgChart.axisLeft
+        leftAxis.textColor = Color.WHITE
+        leftAxis.setDrawGridLines(false)
+        //leftAxis.axisMaximum = 10000f
+        //leftAxis.axisMinimum = 0f
+        leftAxis.setLabelCount(5, true)
+        leftAxis.setDrawGridLines(true)
+
+        val rightAxis: YAxis = ecgChart.axisRight
+        rightAxis.isEnabled = false
+
+        ecgChart.axisLeft.setDrawGridLines(true)
+        ecgChart.xAxis.setDrawGridLines(true)
+        ecgChart.setDrawBorders(true)
+
+        feedMultiple()
     }
 
-    private fun readHealthData() {
-        //read ecg
-        var file = resources.openRawResource(R.raw.ecg)
-        var rows = csvReader().readAll(file)
-        for (i in rows.indices) {
-            val time = rows[i][0].toDouble() / 1000
-            ecgSeries.appendData(DataPoint(time, rows[i][2].toDouble()),true, rows.size)
+    private fun createSet(): LineDataSet? {
+        val set = LineDataSet(null, "Dynamic Data")
+        set.axisDependency = YAxis.AxisDependency.LEFT
+        set.lineWidth = 3f
+        set.color = Color.MAGENTA
+        set.isHighlightEnabled = false
+        set.setDrawValues(false)
+        set.setDrawCircles(false)
+        set.mode = LineDataSet.Mode.CUBIC_BEZIER
+        set.cubicIntensity = 0.2f
+        return set
+    }
+
+    private fun addEntry(ECGdata: ECGDataPacket) {
+        var data: LineData = ecgChart.data
+
+        if (data != null) {
+            var set = data.getDataSetByIndex(0)
+            // set.addEntry(...); // can be called as well
+
+            // set.addEntry(...); // can be called as well
+            if (set == null) {
+                set = createSet()
+                data.addDataSet(set)
+            }
+
+            for (i in ECGdata.payload.streamData) {
+                if (i != null) {
+                    //if (i.timestamp.toFloat() > data.xMax) {
+                        //data.addEntry(Entry(i.timestamp.toFloat(), i.ecgData.toFloat()), 0)
+                    data.addEntry(Entry(prevX++.toFloat(), i.ecgData.toFloat()), 0)
+                    //}
+                }
+            }
+            //data.addEntry(Entry(set.entryCount.toFloat(), (Math.random() * 80).toFloat() + 10f), 0)
+            //Collections.sort(data, EntryXComparator())
+            data.notifyDataChanged()
+
+            // let the chart know it's data has changed
+            ecgChart.notifyDataSetChanged()
+
+            // limit the number of visible entries
+            ecgChart.setVisibleXRangeMaximum(150F)
+            // mChart.setVisibleYRange(30, AxisDependency.LEFT)
+
+            // move to the latest entry
+            ecgChart.moveViewToX(data.getEntryCount().toFloat())
+
+            //println(data.dataSets.toString())
         }
+    }
+
+    private fun feedMultiple() {
+        if (thread != null) {
+            thread.interrupt()
+        }
+        val ecg = com.example.vsmwatchandroidapplication.watchSdk!!.ecgApplication
+        ecg.setCallback { ECGdata ->
+
+            //Log.d("Connection", "DATA :: ${ECGdata.payload.ecgInfo}")
+            runOnUiThread {
+                addEntry(ECGdata)
+            }
+            try {
+                Thread.sleep(10)
+            } catch (e: InterruptedException) {
+                // TODO Auto-generated catch block
+                e.printStackTrace()
+            }
+        }
+
+        ecg.startSensor()
+        ecg.subscribeStream()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (thread != null) {
+            thread.interrupt()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        thread.interrupt()
+        super.onDestroy()
     }
 }
